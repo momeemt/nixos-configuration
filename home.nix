0a2e8ce1cab2a87669fe0ebfa6f15173e52ca8a0
momeemt <momiimt@gmail.com>
2023-08-17 20:45:29 +0900

    add: neovim configure

--- home.nix
+++ home.nix
@@ -53,11 +53,25 @@ in {
       set shiftwidth=2
       set expandtab
       set hls
+
+      augroup vimrc-auto-mkdir
+        autocmd!
+        autocmd BufWritePre * call s:auto_mkdir(expand('<afile>:p:h'), v:cmdbang)
+
+        function! s:auto_mkdir(dir, force)
+          if !isdirectory(a:dir) && (a:force || input(printf('"%s" does not exist. Create? [y/N]', a:dir)) =~? '^y\%[es]$')
+            call mkdir(iconv(a:dir, &encoding, &termencoding), 'p')
+          endif
+        endfunction
+      augroup END
     '';
     plugins = with pkgs.vimPlugins; [
       vim-nix
       nim-vim
       vim-commentary
+      nvim-cmp
+      luasnip
+      nvim-treesitter-parsers.yaml
       {
         plugin = vim-indent-guides;
         config = ''
