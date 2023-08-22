b7928801e30c8068fa4f874083d65cc08848afb8
momeemt <momiimt@gmail.com>
2023-08-22 14:18:26 +0900

    add: zsh configuration files

--- nix/zsh.nix
+++ nix/zsh.nix
@@ -1,8 +1,16 @@
+{ pkgs, ... }:
 {
   programs.zsh = {
+    autocd = true;
     enable = true;
     enableAutosuggestions = true;
     enableCompletion = true;
     enableSyntaxHighlighting = true;
+    defaultKeymap = "vicmd";
+
+    initExtra = builtins.readFile ../zsh/zshrc;
+    envExtra = builtins.readFile ../zsh/zshenv;
+    logoutExtra = builtins.readFile ../zsh/zlogout;
+    profileExtra = builtins.readFile ../zsh/zprofile;
   };
 }
--- zsh/.zshrc
+++
@@ -1,8 +0,0 @@
-autoload -Uz compinit && compinit -u
-autoload -Uz colors && colors
-
-zstyle ':completion:*' menu select
-zstyle ":completion:*:commands" rehash 1
-
-setopt correct
-setopt HIST_IGNORE_DUPS
