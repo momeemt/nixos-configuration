9b779c203f4a1dda0ac32e388ca650d302232b26
momeemt <momiimt@gmail.com>
2023-08-26 14:14:47 +0900

    add: git configuration

--- nix/git.nix
+++ nix/git.nix
@@ -3,6 +3,22 @@
     enable = true;
     userName = "Mutsuha Asada";
     userEmail = "me@momee.mt";
+    aliases = {
+      st = "status";
+      d = "diff";
+      a = "add";
+      aa = "add --all";
+      cm = "commit -m";
+    };
+    ignores = [
+      ".DS_Store"
+      ".direnv"
+      ".envrc"
+    ];
+    difftastic = {
+      enable = true;
+      background = "light";
+    };
   };
 }
 
