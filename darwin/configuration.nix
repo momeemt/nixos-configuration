48fa743361e480868581c5743ff9dc3dc6780c6c
momeemt <momiimt@gmail.com>
2023-08-27 13:32:26 +0900

    add: networking configuration

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -15,6 +15,28 @@
     package = pkgs.nix;
   };
 
+  networking = {
+    computerName = "uguisu";
+    dns = [
+      "1.1.1.1"
+      "1.0.0.1"
+      "8.8.8.8"
+      "8.8.4.4"
+      "2606:4700:4700::1111"
+      "2606:4700:4700::1001"
+      "2001:4860:4860::8888"
+      "2001:4860:4860::8844"
+    ];
+    hostName = "uguisu";
+    knownNetworkServices = [
+      "USB 10/100/1000 LAN"
+      "USB 10/100/1000 LAN 2"
+      "Thunderbolt Bridge"
+      "Wi-Fi"
+    ];
+    localHostName = "uguisu";
+  };
+
   services = {
     nix-daemon = {
       enable = true;
