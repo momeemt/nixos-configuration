8c8c95d53a8da9c6d5ecf52bb38590995c0cee44
momeemt <momiimt@gmail.com>
2023-08-27 14:17:47 +0900

    add: finder configuration

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -73,6 +73,19 @@
         wvous-tr-corner = 12;
       };
 
+      finder = {
+        AppleShowAllExtensions = true;
+        AppleShowAllFiles = true;
+        CreateDesktop = true;
+        # FXDefaultSearchScope = "SCcf";
+        FXEnableExtensionChangeWarning = false;
+        FXPreferredViewStyle = "icnv";
+        QuitMenuItem = true;
+        ShowPathbar = true;
+        ShowStatusBar = true;
+        _FXShowPosixPathInTitle = false;
+      };
+
       NSGlobalDomain = {
         AppleShowAllExtensions = true;
         AppleShowAllFiles = true;
