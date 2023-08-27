5c630b62848046bcdf18f5f65615f51c5c61ec16
momeemt <momiimt@gmail.com>
2023-08-27 14:06:02 +0900

    add: dock configuration

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -45,6 +45,34 @@
 
   system = {
     defaults = {
+      dock = {
+        enable-spring-load-actions-on-all-items = false;
+        appswitcher-all-displays = false;
+        autohide = true;
+        autohide-delay = 0.0;
+        autohide-time-modifier = 1.0;
+        dashboard-in-overlay = false;
+        expose-animation-duration = 1.0;
+        expose-group-by-app = false;
+        largesize = 16;
+        launchanim = true;
+        magnification = false;
+        mineffect = "genie";
+        minimize-to-application = false;
+        mouse-over-hilite-stack = false;
+        mru-spaces = true;
+        orientation = "bottom";
+        show-process-indicators = true;
+        show-recents = false;
+        showhidden = true;
+        static-only = false;
+        tilesize = 48;
+        wvous-bl-corner = 11;
+        wvous-br-corner = 7;
+        wvous-tl-corner = 10;
+        wvous-tr-corner = 12;
+      };
+
       NSGlobalDomain = {
         AppleShowAllExtensions = true;
         AppleShowAllFiles = true;
