581298327e18668c60d7e0b689ae247f10b85803
momeemt <momiimt@gmail.com>
2023-08-27 14:24:11 +0900

    refactoring: dawrin/*.nix

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }:
 {
   imports = [
+    ./dock.nix
+    ./finder.nix
+    ./networking.nix
     ./yabai.nix
     ./skhd.nix
   ];
@@ -15,28 +18,6 @@
     package = pkgs.nix;
   };
 
-  networking = {
-    computerName = "uguisu";
-    dns = [
-      "1.1.1.1"
-      "1.0.0.1"
-      "8.8.8.8"
-      "8.8.4.4"
-      "2606:4700:4700::1111"
-      "2606:4700:4700::1001"
-      "2001:4860:4860::8888"
-      "2001:4860:4860::8844"
-    ];
-    hostName = "uguisu";
-    knownNetworkServices = [
-      "USB 10/100/1000 LAN"
-      "USB 10/100/1000 LAN 2"
-      "Thunderbolt Bridge"
-      "Wi-Fi"
-    ];
-    localHostName = "uguisu";
-  };
-
   services = {
     nix-daemon = {
       enable = true;
@@ -45,47 +26,6 @@
 
   system = {
     defaults = {
-      dock = {
-        enable-spring-load-actions-on-all-items = false;
-        appswitcher-all-displays = false;
-        autohide = true;
-        autohide-delay = 0.0;
-        autohide-time-modifier = 1.0;
-        dashboard-in-overlay = false;
-        expose-animation-duration = 1.0;
-        expose-group-by-app = false;
-        largesize = 16;
-        launchanim = true;
-        magnification = false;
-        mineffect = "genie";
-        minimize-to-application = false;
-        mouse-over-hilite-stack = false;
-        mru-spaces = true;
-        orientation = "bottom";
-        show-process-indicators = true;
-        show-recents = false;
-        showhidden = true;
-        static-only = false;
-        tilesize = 48;
-        wvous-bl-corner = 11;
-        wvous-br-corner = 3;
-        wvous-tl-corner = 10;
-        wvous-tr-corner = 12;
-      };
-
-      finder = {
-        AppleShowAllExtensions = true;
-        AppleShowAllFiles = true;
-        CreateDesktop = true;
-        # FXDefaultSearchScope = "SCcf";
-        FXEnableExtensionChangeWarning = false;
-        FXPreferredViewStyle = "icnv";
-        QuitMenuItem = true;
-        ShowPathbar = true;
-        ShowStatusBar = true;
-        _FXShowPosixPathInTitle = false;
-      };
-
       NSGlobalDomain = {
         AppleShowAllExtensions = true;
         AppleShowAllFiles = true;
---
+++ darwin/finder.nix
@@ -0,0 +1,15 @@
+{
+  finder = {
+    AppleShowAllExtensions = true;
+    AppleShowAllFiles = true;
+    CreateDesktop = true;
+    # FXDefaultSearchScope = "SCcf";
+    FXEnableExtensionChangeWarning = false;
+    FXPreferredViewStyle = "icnv";
+    QuitMenuItem = true;
+    ShowPathbar = true;
+    ShowStatusBar = true;
+    _FXShowPosixPathInTitle = false;
+  };
+}
+
