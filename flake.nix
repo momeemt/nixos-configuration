6141a0f89424bd204c711a42c801c723e168ac93
momeemt <momiimt@gmail.com>
2023-11-27 22:01:24 +0900

    update: flakes

--- flake.nix
+++ flake.nix
@@ -1,20 +1,14 @@
 {
   inputs = {
-    nixpkgs = {
-      owner = "NixOS";
-      repo = "nixpkgs";
-      rev = "959d8a84bff0f9bff8525b9b074085269ddf5d79";
-      type = "github";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/master";
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
     flake-utils = {
       owner = "numtide";
       repo = "flake-utils";
       rev = "919d646de7be200f3bf08cb76ae1f09402b6f9b4";
       type = "github";
-    };
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-darwin = {
       url = "github:LnL7/nix-darwin/master";
