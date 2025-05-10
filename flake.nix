669356147b85b13130e7673488bd7338b7606d17
Mutsuha Asada <me@momee.mt>
2025-05-10 18:50:30 +0900

    add: flake-parts, treefmt, and other modules to flake.nix

--- flake.nix
+++ flake.nix
@@ -2,44 +2,99 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    darwin.url = "github:LnL7/nix-darwin";
-    darwin.inputs.nixpkgs.follows = "nixpkgs-darwin";
-    flake-utils.url = "github:numtide/flake-utils";
-    sops-nix.url = "github:Mic92/sops-nix";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin/nix-darwin-24.11";
+      inputs.nixpkgs.follows = "nixpkgs-darwin";
+    };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    git-hooks-nix = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixvim = {
+      url = "github:nix-community/nixvim/nixos-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = {
-    nixpkgs,
-    home-manager,
-    darwin,
-    sops-nix,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem
-    (
-      system: let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-      in {
-        devShell = pkgs.mkShell {
+  outputs = {flake-parts, ...} @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = with inputs; [
+        treefmt-nix.flakeModule
+        git-hooks-nix.flakeModule
+      ];
+
+      systems = import inputs.systems;
+
+      perSystem = {
+        pkgs,
+        config,
+        ...
+      }: {
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [
+            config.treefmt.build.devShell
+          ];
           buildInputs = with pkgs; [
             nil
-            alejandra
             lua-language-server
             nodePackages.vim-language-server
           ];
         };
-      }
-    )
-    // {
-      nixosConfigurations = {
-        emu = nixpkgs.lib.nixosSystem {
+
+        treefmt = {
+          projectRootFile = "flake.nix";
+          programs = {
+            actionlint.enable = true;
+            alejandra.enable = true;
+            deadnix.enable = true;
+            mdformat.enable = true;
+            shellcheck.enable = true;
+            shfmt = {
+              enable = true;
+              includes = [
+                "*.sh"
+                "*.bash"
+                "*.envrc"
+                "*.envrc.*"
+                "*.zsh"
+                "**/zshrc"
+                "**/zprofile"
+              ];
+            };
+            statix.enable = true;
+            stylua.enable = true;
+            yamlfmt.enable = true;
+          };
+          settings.global.excludes = [
+            "LICENSE-*"
+            "secrets/secrets.yml"
+            ".github/CODEOWNERS"
+            ".gitattributes"
+            "modules/tmux/tmux.conf"
+            "*.vim"
+            "Makefile"
+          ];
+        };
+      };
+
+      flake.nixosConfigurations = {
+        emu = inputs.nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
-          modules = [
+          modules = with inputs; [
             ./hosts/emu
             home-manager.nixosModules.home-manager
             {
@@ -51,9 +106,9 @@
           ];
         };
         # system security lab.
-        oshidori = nixpkgs.lib.nixosSystem {
+        oshidori = inputs.nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
-          modules = [
+          modules = with inputs; [
             ./hosts/oshidori
             home-manager.nixosModules.home-manager
             {
@@ -65,10 +120,10 @@
         };
       };
 
-      darwinConfigurations = {
-        uguisu = darwin.lib.darwinSystem {
+      flake.darwinConfigurations = {
+        uguisu = inputs.nixpkgs-darwin.lib.darwinSystem {
           system = "aarch64-darwin";
-          modules = [
+          modules = with inputs; [
             ./hosts/uguisu
             home-manager.darwinModules.home-manager
             {
