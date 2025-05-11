0eafe0c3206fe6cca44b8bedf07f46a6b1291e32
Mutsuha Asada <me@momee.mt>
2025-05-11 18:42:50 +0900

    chore: nix fmt

--- flake.nix
+++ flake.nix
@@ -114,7 +114,7 @@
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
-              home-manager.extraSpecialArgs = { inherit inputs; };
+              home-manager.extraSpecialArgs = {inherit inputs;};
               home-manager.users.momeemt = import ./home/oshidori;
             }
           ];
--- home/oshidori/default.nix
+++ home/oshidori/default.nix
@@ -1,4 +1,7 @@
-{pkgs, inputs, ...}: {
+{
+  pkgs,
+  ...
+}: {
   imports = [
     ../../modules/alacritty
     ../../modules/direnv
