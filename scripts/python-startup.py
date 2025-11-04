9de87c6f554de5ad8cab89ad05e69ffe71515b1f
Mutsuha Asada <me@momee.mt>
2025-11-04 23:40:40 +0900

    wip: clean dotfiles in homeDirectory

---
+++ scripts/python-startup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import os, atexit, readline, pathlib
+
+xdg_state = os.getenv("XDG_STATE_HOME", os.path.expanduser("~/.local/state"))
+hist_dir = os.path.join(xdg_state, "python")
+pathlib.Path(hist_dir).mkdir(parents=True, exist_ok=True)
+histfile = os.path.join(hist_dir, "history")
+
+try:
+    readline.read_history_file(histfile)
+except:
+    pass
+
+atexit.register(readline.write_history_file, histfile)
+
