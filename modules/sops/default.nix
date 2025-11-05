f28aafdac4c33e1d63821f60492ff5dd63d8fcbd
Mutsuha Asada <me@momee.mt>
2025-11-05 10:18:57 +0900

    wip: fix to try using /etc/ssh/ssh_host_ed25519_key

--- modules/sops/default.nix
+++ modules/sops/default.nix
@@ -3,6 +3,7 @@
     age = {
       generateKey = true;
       keyFile = "/var/lib/sops-nix/key.txt";
+      sshKeyPaths = [];
     };
     defaultSopsFile = ../../secrets/secrets.enc.yml;
     secrets = {
