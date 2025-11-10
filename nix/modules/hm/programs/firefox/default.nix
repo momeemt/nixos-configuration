85354d7ac4135ce94473773a1d57e6b375f6708e
Mutsuha Asada <me@momee.mt>
2025-11-10 22:25:19 +0900

    treewide: treefmt

--- nix/modules/hm/programs/firefox/default.nix
+++ nix/modules/hm/programs/firefox/default.nix
@@ -78,11 +78,13 @@
           };
           nixos-wiki = {
             name = "NixOS Wiki";
-            urls = [{
-              template = "https://wiki.nixos.org/w/index.php?search={searchTerms}";
-            }];
+            urls = [
+              {
+                template = "https://wiki.nixos.org/w/index.php?search={searchTerms}";
+              }
+            ];
             iconMapObj."16" = "https://wiki.nixos.org/favicon.ico";
-            definedAliases = [ "@nw" ];
+            definedAliases = ["@nw"];
           };
           github = {
             name = "GitHub";
