d8fafeb77bef8e8755f5d6d83401f29ca8df1646
Mutsuha Asada <me@momee.mt>
2025-11-11 12:03:45 +0900

    treewide: treefmt

--- nix/packages/overlay.nix
+++ nix/packages/overlay.nix
@@ -1,4 +1,4 @@
-self: super: let
+_self: super: let
   pkgs = super;
   subscribePackages = import ./subscribe {inherit pkgs;};
 in {
@@ -9,8 +9,8 @@ in {
     quitapp = import ./quitapp {inherit pkgs;};
     switch-config-branch = import ./switch-config-branch {inherit pkgs;};
     updatekeys-secrets = import ./updatekeys-secrets {inherit pkgs;};
-    ok = subscribePackages.ok;
-    ng = subscribePackages.ng;
-    subscribe = subscribePackages.subscribe;
+    inherit (subscribePackages) ok;
+    inherit (subscribePackages) ng;
+    inherit (subscribePackages) subscribe;
   };
 }
