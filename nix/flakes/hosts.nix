ad083bc4a9ebf64725caff13bcec84ad5fe3798a
Mutsuha Asada <me@momee.mt>
2025-11-17 11:21:07 +0900

    fix: statix

--- nix/flakes/hosts.nix
+++ nix/flakes/hosts.nix
@@ -12,113 +12,115 @@
     inherit (masterPkgs) vscode vscode-with-extensions;
   };
 in {
-  flake.nixosConfigurations = {
-    emu = let
-      system = "x86_64-linux";
-    in
-      withSystem system ({pkgs, ...}: let
-        siteLib = import ../lib {
-          inherit pkgs system;
-          inherit (pkgs) lib;
-        };
+  flake = {
+    nixosConfigurations = {
+      emu = let
+        system = "x86_64-linux";
       in
-        Hosts.mkNixos {
-          inherit system siteLib;
-          hostPath = ../hosts/emu;
-          users = {
-            momeemt = ../home/emu;
+        withSystem system ({pkgs, ...}: let
+          siteLib = import ../lib {
+            inherit pkgs system;
+            inherit (pkgs) lib;
           };
-          extraModules = with inputs; [
-            NixVirt.nixosModules.default
-            vscode-server.nixosModules.default
-            comin.nixosModules.comin
-          ];
-          overlays = [vscodeOverlay];
-        });
+        in
+          Hosts.mkNixos {
+            inherit system siteLib;
+            hostPath = ../hosts/emu;
+            users = {
+              momeemt = ../home/emu;
+            };
+            extraModules = with inputs; [
+              NixVirt.nixosModules.default
+              vscode-server.nixosModules.default
+              comin.nixosModules.comin
+            ];
+            overlays = [vscodeOverlay];
+          });
 
-    shime = let
-      system = "x86_64-linux";
-    in
-      withSystem system ({pkgs, ...}: let
-        siteLib = import ../lib {
-          inherit pkgs system;
-          inherit (pkgs) lib;
-        };
+      shime = let
+        system = "x86_64-linux";
       in
-        Hosts.mkNixos {
-          inherit system siteLib;
-          hostPath = ../hosts/shime;
-          users = {
-            momeemt = ../home/shime;
+        withSystem system ({pkgs, ...}: let
+          siteLib = import ../lib {
+            inherit pkgs system;
+            inherit (pkgs) lib;
           };
-          extraModules = with inputs; [
-            NixVirt.nixosModules.default
-            vscode-server.nixosModules.default
-            comin.nixosModules.comin
-          ];
-          overlays = [vscodeOverlay];
-        });
+        in
+          Hosts.mkNixos {
+            inherit system siteLib;
+            hostPath = ../hosts/shime;
+            users = {
+              momeemt = ../home/shime;
+            };
+            extraModules = with inputs; [
+              NixVirt.nixosModules.default
+              vscode-server.nixosModules.default
+              comin.nixosModules.comin
+            ];
+            overlays = [vscodeOverlay];
+          });
 
-    oshidori = let
-      system = "x86_64-linux";
-    in
-      withSystem system ({pkgs, ...}: let
-        siteLib = import ../lib {
-          inherit pkgs system;
-          inherit (pkgs) lib;
-        };
+      oshidori = let
+        system = "x86_64-linux";
       in
-        Hosts.mkNixos {
-          inherit system siteLib;
-          hostPath = ../hosts/oshidori;
-          users = {
-            momeemt = ../home/oshidori;
+        withSystem system ({pkgs, ...}: let
+          siteLib = import ../lib {
+            inherit pkgs system;
+            inherit (pkgs) lib;
           };
-          extraModules = with inputs; [
-            vscode-server.nixosModules.default
-            comin.nixosModules.comin
-          ];
-          overlays = [vscodeOverlay];
-        });
-  };
+        in
+          Hosts.mkNixos {
+            inherit system siteLib;
+            hostPath = ../hosts/oshidori;
+            users = {
+              momeemt = ../home/oshidori;
+            };
+            extraModules = with inputs; [
+              vscode-server.nixosModules.default
+              comin.nixosModules.comin
+            ];
+            overlays = [vscodeOverlay];
+          });
+    };
 
-  flake.darwinConfigurations = {
-    uguisu = let
-      system = "aarch64-darwin";
-    in
-      withSystem system ({pkgs, ...}: let
-        siteLib = import ../lib {
-          inherit pkgs system;
-          inherit (pkgs) lib;
-        };
+    darwinConfigurations = {
+      uguisu = let
+        system = "aarch64-darwin";
       in
-        Hosts.mkDarwin {
-          inherit system siteLib;
-          hostPath = ../hosts/uguisu;
-          users = {
-            momeemt = ../home/uguisu;
+        withSystem system ({pkgs, ...}: let
+          siteLib = import ../lib {
+            inherit pkgs system;
+            inherit (pkgs) lib;
           };
-          overlays = [inputs.brew-nix.overlays.default];
-        });
-  };
+        in
+          Hosts.mkDarwin {
+            inherit system siteLib;
+            hostPath = ../hosts/uguisu;
+            users = {
+              momeemt = ../home/uguisu;
+            };
+            overlays = [inputs.brew-nix.overlays.default];
+          });
+    };
 
-  flake.homeConfigurations = {
-    example = let
-      system = builtins.currentSystem;
-    in
-      withSystem system ({pkgs, ...}: let
-        siteLib = import ../lib {
-          inherit pkgs system;
-          inherit (pkgs) lib;
-        };
+    homeConfigurations = {
+      example = let
+        system = builtins.currentSystem;
       in
-        inputs.home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = [
-            (_: {nixpkgs.overlays = [(import ../packages/overlay.nix)];})
-            ../home/example
-          ];
-          extraSpecialArgs = {inherit inputs siteLib;};
-        });
+        withSystem system ({pkgs, ...}: let
+          siteLib = import ../lib {
+            inherit pkgs system;
+            inherit (pkgs) lib;
+          };
+        in
+          inputs.home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            modules = [
+              (_: {nixpkgs.overlays = [(import ../packages/overlay.nix)];})
+              ../home/example
+            ];
+            extraSpecialArgs = {inherit inputs siteLib;};
+          });
+    };
   };
 }
