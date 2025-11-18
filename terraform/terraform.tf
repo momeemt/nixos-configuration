8dd0e8c8dbd094390c0611acaa2214f1c3f9fdd8
Mutsuha Asada <me@momee.mt>
2025-11-18 12:59:21 +0900

    add: cloudflare dns records

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,17 +1,17 @@
 terraform {
-    required_version = "1.13.5"
+  required_version = "1.13.5"
 
-    required_providers {
-      cloudflare = {
-        source = "cloudflare/cloudflare"
-        version = "5.12.0"
-      }
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "5.12.0"
     }
+  }
 
-    cloud {
-        organization = "momeemt"
-        workspaces {
-          name = "config"
-        }
+  cloud {
+    organization = "momeemt"
+    workspaces {
+      name = "config"
     }
+  }
 }
