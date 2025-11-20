218a39d5800bfa589796ae2008eb75754dabaa4b
Mutsuha Asada <me@momee.mt>
2025-11-20 21:07:36 +0900

    fix: forget to import sops modules

--- nix/home/shime/default.nix
+++ nix/home/shime/default.nix
@@ -1,11 +1,13 @@
 {inputs, ...}: {
   imports = [
     inputs.nixvim.homeManagerModules.nixvim
+    inputs.sops-nix.homeManagerModules.sops
     ../../modules/nixvim
     inputs.tmux-nix.homeModules.tmux-nix
     ../../modules/tmux-nix
     ../../modules/hm/nix
     ../../modules/hm/programs/only-cli.nix
+    ../../modules/hm/sops
     ../../modules/hm/wakatime
     ../../modules/site/home
   ];
