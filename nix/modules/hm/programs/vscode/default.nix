3aef795e11e873a1ec48738b25a057356921bbea
Mutsuha Asada <me@momee.mt>
2025-11-21 15:02:18 +0900

    add: support XDG Base Directory for VSCode

--- nix/modules/hm/programs/vscode/default.nix
+++ nix/modules/hm/programs/vscode/default.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   programs.vscode = {
     enable = true;
+    package = pkgs.vscode;
     profiles.default.extensions = with pkgs.vscode-extensions; [
       tobiasalthoff.atom-material-theme
       ms-vscode-remote.remote-containers
@@ -11,7 +12,6 @@
       arrterian.nix-env-selector
       kamadorueda.alejandra
       vscodevim.vim
-      # nvarner.typst-lsp
       wakatime.vscode-wakatime
     ];
   };
--- nix/modules/site/home/default.nix
+++ nix/modules/site/home/default.nix
@@ -160,6 +160,7 @@ in {
           GOPATH = "${dataHome}/go";
           GOMODCACHE = "${cacheHome}/go/mod";
           GOCACHE = "${cacheHome}/go/build";
+          VSCODE_PORTABLE = "${dataHome}/vscode";
           # https://matplotlib.org/stable/api/matplotlib_configuration_api.html#matplotlib.get_configdir
           MPLCONFIGDIR = "${configHome}/matplotlib";
           # https://doc.rust-lang.org/cargo/reference/environment-variables.html
