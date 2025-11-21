c25a4a5613714de419d34e3d497c281493f4d5da
Mutsuha Asada <me@momee.mt>
2025-11-21 14:17:56 +0900

    add: support XDG Base Directory for anaconda

--- nix/modules/site/home/default.nix
+++ nix/modules/site/home/default.nix
@@ -153,6 +153,7 @@ in {
           BUNDLE_USER_CONFIG = "${configHome}/bundle";
           BUNDLE_USER_CACHE = "${cacheHome}/bundle";
           BUNDLE_USER_PLUGIN = "${dataHome}/bundle";
+          CONDARC = "${configHome}/conda/condarc";
           # https://matplotlib.org/stable/api/matplotlib_configuration_api.html#matplotlib.get_configdir
           MPLCONFIGDIR = "${configHome}/matplotlib";
           # https://doc.rust-lang.org/cargo/reference/environment-variables.html
