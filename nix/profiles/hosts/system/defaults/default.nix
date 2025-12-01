f40fa801a163818d92fc50eb40f9c09ef43ba014
Mutsuha Asada <me@momee.mt>
2025-12-01 20:06:58 +0900

    add: system.defaults.finder

--- nix/profiles/hosts/system/defaults/default.nix
+++ nix/profiles/hosts/system/defaults/default.nix
@@ -1,6 +1,7 @@
 {
   imports = [
     ./dock
+    ./finder
   ];
 
   system.defaults = {
---
+++ nix/profiles/hosts/system/defaults/finder/default.nix
@@ -0,0 +1,24 @@
+{
+  system.defaults.finder = {
+    AppleShowAllExtensions = true;
+    AppleShowAllFiles = true;
+    CreateDesktop = false;
+    FXDefaultSearchScope = "SCcf"; # target the current directory
+    FXEnableExtensionChangeWarning = false;
+    FXPreferredViewStyle = "Nlsv"; # List
+    FXRemoveOldTrashItems = true;
+    NewWindowTarget = "Home";
+    # NewWindowTargetPath = "~";
+    QuitMenuItem = true;
+    ShowExternalHardDrivesOnDesktop = true;
+    ShowHardDrivesOnDesktop = true;
+    ShowMountedServersOnDesktop = true;
+    ShowPathbar = true;
+    ShowRemovableMediaOnDesktop = true;
+    ShowStatusBar = true;
+    _FXEnableColumnAutoSizing = true;
+    _FXShowPosixPathInTitle = false;
+    _FXSortFoldersFirst = true;
+    _FXSortFoldersFirstOnDesktop = true;
+  };
+}
