d7f8f0714c478b838fd327b706a69b0f0f18878b
Mutsuha Asada <me@momee.mt>
2025-12-02 14:18:41 +0900

    remove: osc52 settings

--- nix/profiles/nixvim/default.nix
+++ nix/profiles/nixvim/default.nix
@@ -7,20 +7,6 @@
 
     colorschemes.vscode.enable = true;
     clipboard.register = "unnamedplus";
-    extraConfigLua = ''
-      local osc52 = require('vim.ui.clipboard.osc52')
-      vim.g.clipboard = {
-        name = 'OSC 52',
-        copy = {
-          ['+'] = osc52.copy('+'),
-          ['*'] = osc52.copy('*'),
-        },
-        paste = {
-          ['+'] = osc52.paste('+'),
-          ['*'] = osc52.paste('*'),
-        },
-      }
-    '';
 
     highlight = {
       Normal = {
