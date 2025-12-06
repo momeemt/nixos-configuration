3e754ce1a3bbf2385dc0ba62f4786da561b0b753
Mutsuha Asada <me@momee.mt>
2025-12-06 14:47:51 +0900

    add: perSystem.treefmt.<name>.package

--- nix/flakes/treefmt.nix
+++ nix/flakes/treefmt.nix
@@ -8,16 +8,53 @@ _: {
       projectRootFile = "flake.nix";
 
       programs = {
-        actionlint.enable = true;
-        alejandra.enable = true;
-        deadnix.enable = true;
-        mdformat.enable = true;
-        ruff-check.enable = true;
-        ruff-format.enable = true;
-        shellcheck.enable = true;
-        shfmt.enable = true;
-        statix.enable = true;
+        # GitHub Actions
+        actionlint = {
+          enable = true;
+          package = pkgs.actionlint;
+        };
 
+        # Nix
+        alejandra = {
+          enable = true;
+          package = pkgs.alejandra;
+        };
+        deadnix = {
+          enable = true;
+          package = pkgs.deadnix;
+        };
+        statix = {
+          enable = true;
+          package = pkgs.statix;
+        };
+
+        # Markdown
+        mdformat = {
+          enable = true;
+          package = pkgs.mdformat;
+        };
+
+        # Python
+        ruff-check = {
+          enable = true;
+          package = pkgs.ruff;
+        };
+        ruff-format = {
+          enable = true;
+          package = pkgs.ruff;
+        };
+
+        # sh/bash
+        shellcheck = {
+          enable = true;
+          package = pkgs.shellcheck;
+        };
+        shfmt = {
+          enable = true;
+          package = pkgs.shfmt;
+        };
+
+        # Lua
         stylua = {
           enable = true;
           package = pkgs.stylua;
@@ -33,8 +70,17 @@ _: {
           };
         };
 
-        terraform.enable = true;
-        yamlfmt.enable = true;
+        # Terraform
+        terraform = {
+          enable = true;
+          package = pkgs.terraform;
+        };
+
+        # YAML
+        yamlfmt = {
+          enable = true;
+          package = pkgs.yamlfmt;
+        };
       };
     };
   };
