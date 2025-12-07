23cdf2b74055fa469403396b6a1d4d40414272bb
Mutsuha Asada <me@momee.mt>
2025-12-07 15:54:16 +0900

    add: action-validator for pre-commit

--- nix/flakes/pre-commit.nix
+++ nix/flakes/pre-commit.nix
@@ -1,13 +1,24 @@
 _: {
   perSystem = {
     config,
+    pkgs,
     lib,
     ...
   }: {
     pre-commit = {
       check.enable = true;
+      inherit pkgs;
       settings = {
+        enable = true;
+        package = pkgs.pre-commit;
+        addGcRoot = true;
+        default_stages = ["pre-commit"];
+        gitPackage = pkgs.gitMinimal;
         hooks = {
+          action-validator = {
+            enable = true;
+            package = pkgs.action-validator;
+          };
           treefmt = {
             enable = true;
             packageOverrides = {
