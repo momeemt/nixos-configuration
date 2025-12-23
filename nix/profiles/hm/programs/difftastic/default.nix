26dc2092f45b063619dafcbb2b9b00e755b81272
Mutsuha Asada <me@momee.mt>
2025-12-23 11:25:50 +0900

    add: difftastic git support

--- nix/profiles/hm/programs/difftastic/default.nix
+++ nix/profiles/hm/programs/difftastic/default.nix
@@ -2,6 +2,10 @@
   programs.difftastic = {
     enable = true;
     package = pkgs.difftastic;
+    git = {
+      enable = true;
+      diffToolMode = true;
+    };
     options = {
       background = "light";
     };
