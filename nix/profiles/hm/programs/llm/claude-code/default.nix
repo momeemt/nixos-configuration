c4c88f1a19d86879e5e1752cb7782968ecb6ed19
Mutsuha Asada <me@momee.mt>
2025-12-28 15:51:19 +0900

    remove: mcpServers

--- nix/profiles/hm/programs/llm/claude-code/default.nix
+++ nix/profiles/hm/programs/llm/claude-code/default.nix
@@ -1,27 +1,6 @@
-{
-  pkgs,
-  inputs,
-  config,
-  ...
-}: {
+{pkgs, ...}: {
   programs.claude-code = {
     enable = true;
     package = pkgs.claude-code;
-    mcpServers = inputs.mcp-servers-nix.lib.mkConfig pkgs {
-      programs = {
-        context7.enable = true;
-        nixos.enable = true;
-      };
-      settings.servers = {
-        mcp-obsidian = {
-          command = "${pkgs.lib.getExe' pkgs.nodejs "npx"}";
-          args = [
-            "-y"
-            "mcp-obsidian"
-            "${config.xdg.dataHome}/ghq/github.com/momeemt/note/note"
-          ];
-        };
-      };
-    };
   };
 }
